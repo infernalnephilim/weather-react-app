91e43e6007d6c4284de554b2ab9a9315e299a3da
Aleksandra Półtorak <xmacabrax@gmail.com>
2018-10-19 22:45:32 +0200

    Updates

---
+++ precache-manifest.97ed7b6266a9b59e6dbd4d736d02bf5c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/weather-react-app/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "d01157d07cdf0ece631f",
+    "url": "/weather-react-app/static/js/runtime~main.d01157d0.js"
+  },
+  {
+    "revision": "3ceead9fd5f1674f367b",
+    "url": "/weather-react-app/static/js/main.3ceead9f.chunk.js"
+  },
+  {
+    "revision": "a866bd1e2925d63fc6c0",
+    "url": "/weather-react-app/static/js/1.a866bd1e.chunk.js"
+  },
+  {
+    "revision": "3ceead9fd5f1674f367b",
+    "url": "/weather-react-app/static/css/main.4f3f3ca5.chunk.css"
+  },
+  {
+    "revision": "1f5c543a87fb556f74aafdf013022447",
+    "url": "/weather-react-app/index.html"
+  }
+];
